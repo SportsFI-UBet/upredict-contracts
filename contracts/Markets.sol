8f220d8d74d6d5c2ac4ae474b9ce28871ddaf699
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2025-02-07 15:50:57 -0500

    Add Implementation

--- contracts/Markets.sol
+++ contracts/Markets.sol
@@ -1,12 +1,19 @@
+// solhint-disable one-contract-per-file
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Context } from "@openzeppelin/contracts/utils/Context.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+
+import { UD60x18, ud60x18, unwrap } from "@prb/math/UD60x18.sol";
+
+// TODO: split among more files
 
 /**
- * Some implementation defined blob describing hidden info of a market
+ * Some implementation defined blob describing hidden info of a market. Struct is there for strong type safety.
  */
 struct MarketBlob {
     bytes data;
@@ -30,6 +37,8 @@ struct ResultBlob {
 
 type ResultCommitment is bytes32;
 
+ResultCommitment constant nullResultCommitment = ResultCommitment.wrap(bytes32(0));
+
 /**
  * Some implementation defined blob describing hidden info of bet
  */
@@ -39,19 +48,54 @@ struct BetBlob {
 
 type BetCommitment is bytes32;
 
+function betCommitmentEq(BetCommitment a, BetCommitment b) pure returns (bool) {
+    return BetCommitment.unwrap(a) == BetCommitment.unwrap(b);
+}
+
+function resultCommitmentEq(ResultCommitment a, ResultCommitment b) pure returns (bool) {
+    return ResultCommitment.unwrap(a) == ResultCommitment.unwrap(b);
+}
+
+function marketCommitmentEq(MarketCommitment a, MarketCommitment b) pure returns (bool) {
+    return MarketCommitment.unwrap(a) == MarketCommitment.unwrap(b);
+}
+
+function betCommitmentNeq(BetCommitment a, BetCommitment b) pure returns (bool) {
+    return BetCommitment.unwrap(a) != BetCommitment.unwrap(b);
+}
+
+function resultCommitmentNeq(ResultCommitment a, ResultCommitment b) pure returns (bool) {
+    return ResultCommitment.unwrap(a) != ResultCommitment.unwrap(b);
+}
+
+function marketCommitmentNeq(MarketCommitment a, MarketCommitment b) pure returns (bool) {
+    return MarketCommitment.unwrap(a) != MarketCommitment.unwrap(b);
+}
+
+using { betCommitmentEq as ==, betCommitmentNeq as != } for BetCommitment global;
+
+using { resultCommitmentEq as ==, resultCommitmentNeq as != } for ResultCommitment global;
+
+using { marketCommitmentEq as ==, marketCommitmentNeq as != } for MarketCommitment global;
+
 interface MarketsErrors {
-    error MarketsInvalidUserNonce(uint256 nonce);
+    error MarketsWrongSender(address sender);
+    error MarketsInvalidUserNonce(address user, uint256 nonce);
+
     error MarketsResultAlreadyRevealed(MarketCommitment marketCommitment, ResultCommitment resultCommitment);
+    error MarketsResultTooEarly(MarketCommitment marketCommitment, uint256 blockNumber);
+    error MarketsInvalidResult(MarketCommitment marketCommitment, ResultCommitment resultCommitment);
+
+    error MarketsInconsistentResult(MarketCommitment marketCommitment, ResultCommitment resultCommitment);
 }
 
 // TODO: ERC2771 Context? for metatx
-// TODO: Access control
-abstract contract MarketsBase is Context, MarketsErrors {
+abstract contract MarketsBase is Context, MarketsErrors, AccessControl {
     using SafeERC20 for IERC20;
 
     // TODO: Is packing worth it? Measure gas cost
     struct BetRequest {
-        // TODO: ERC20 permit?
+        // TODO: implementation version? Make sure it matches the contract implementation
         IERC20 token;
         uint96 amount;
         address from; // who is making the bet
@@ -60,17 +104,13 @@ abstract contract MarketsBase is Context, MarketsErrors {
          * block deadline when user can submit bet (needed?)
          */
         uint256 submissionDeadlineBlock;
-        // TODO: refund deadline?
-        /**
-         * a commitment for hidden information of bet. Can be hash of the blob associated with the bet
-         */
-        BetCommitment commitment;
     }
+    // TODO: refund deadline?
 
     /**
      * Stored on the blockchain to reference during reveal phase
      */
-    struct BetInfo {
+    struct BetState {
         // TODO: not sure what is needed here beside the fact that "it exists"
         uint256 amount;
     }
@@ -86,30 +126,53 @@ abstract contract MarketsBase is Context, MarketsErrors {
 
     // TODO: If we store MarketHash, the marketId can still be hidden.
     // - Makes it easier to track bets for the same market, without knowing which exact market it is
-    mapping(MarketCommitment => ResultCommitment) marketResults;
-    mapping(BetCommitment => BetInfo) bets;
+    mapping(MarketCommitment => ResultCommitment) public marketResults;
+    mapping(BetCommitment => BetState) public bets;
 
     /**
      * Increments every time a user places a bet
      */
-    mapping(address => uint256) userNonces;
+    mapping(address => uint256) public userNonces;
+
+    constructor(address admin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    }
 
     /**
      * Place a bet according to the request, signed offchain by the backend.
-     *
+     * @param bet the publicly visible bet details
+     * @param betCommitment commitment for the hidden portion of bet information
      */
-    function placeBet(BetRequest calldata bet) external {
-        bet.token.safeTransferFrom(_msgSender(), address(this), bet.amount);
+    function placeBet(BetRequest calldata bet, BetCommitment betCommitment) external {
+        // TODO: need signature from backend
+        if (_msgSender() != bet.from) {
+            revert MarketsWrongSender(_msgSender());
+        }
 
-        bets[bet.commitment] = BetInfo({ amount: bet.amount });
+        bets[betCommitment] = BetState({ amount: bet.amount });
 
-        // TODO: check nonce and increment
+        // Check user nonce to avoid replay attacks
+        uint256 expectedNonce = userNonces[bet.from];
+        if (bet.nonce != expectedNonce) {
+            revert MarketsInvalidUserNonce(bet.from, bet.nonce);
+        }
+        userNonces[bet.from]++;
+
+        // TODO: betCommitment must include the BetRequest as part of it - i.e. BetBlob includes BetRequest by default
+
+        // TODO: take care of fees
 
         // TODO:
         // Need to prevent bets to be entered past the market deadline without revealing the deadline or the market.
         // - if we sign the blob with some specific private key and do ecrecover, we can make backend arbiter of bets.
         //   Therefore need to store AccessControl permissions for addresses
         //   recovered through ecrecover. Can enable several signers to sign
+
+        // TODO: any hooks needed for the derived contract?
+
+        bet.token.safeTransferFrom(bet.from, address(this), bet.amount);
+
+        // TODO: event
     }
 
     /**
@@ -118,25 +181,29 @@ abstract contract MarketsBase is Context, MarketsErrors {
     function revealMarketResult(
         MarketBlob calldata marketBlob,
         ResultBlob calldata resultBlob,
-        bytes calldata /* resultSignature */
+        bytes calldata resultSignature // TODO: this signature should encompass both market and result blobs
     ) external {
-        // TODO: verify resultSignature
+        // Should this be an EIP-712 signature?
         // TODO: avoid replay attack if first time rejected
 
         MarketCommitment marketCommitment = MarketCommitment.wrap(keccak256(marketBlob.data));
         ResultCommitment resultCommitment = ResultCommitment.wrap(keccak256(resultBlob.data));
+        address signerAddress = ECDSA.recover(ResultCommitment.unwrap(resultCommitment), resultSignature);
+        _checkRole(RESULT_SIGNATURE_ROLE, signerAddress);
+
         ResultCommitment existingCommitment = marketResults[marketCommitment];
-        // TODO: add library for operations
-        if (ResultCommitment.unwrap(existingCommitment) == ResultCommitment.unwrap(resultCommitment)) {
+        if (existingCommitment == resultCommitment) {
             return;
         }
-        if (ResultCommitment.unwrap(existingCommitment) != bytes32(0)) {
+        if (existingCommitment != nullResultCommitment) {
             revert MarketsResultAlreadyRevealed(marketCommitment, existingCommitment);
         }
         // hook for implementation to verify that the result makes sense given all the bets
-        _verifyResult(marketBlob, resultBlob);
+        _verifyResult(marketCommitment, marketBlob, resultCommitment, resultBlob);
 
         marketResults[marketCommitment] = resultCommitment;
+
+        // TODO: event
     }
 
     /**
@@ -146,14 +213,24 @@ abstract contract MarketsBase is Context, MarketsErrors {
         public
         returns (IERC20 token, address to, uint256 amount)
     {
-        // TODO: verify market and result match their commitments
+        MarketCommitment marketCommitment = MarketCommitment.wrap(keccak256(marketBlob.data));
+        ResultCommitment resultCommitment = ResultCommitment.wrap(keccak256(resultBlob.data));
+        ResultCommitment existingCommitment = marketResults[marketCommitment];
+        if (existingCommitment != resultCommitment) {
+            revert MarketsInconsistentResult(marketCommitment, resultCommitment);
+        }
+
         // TODO: think about re-entrancy
         // TODO: think about repeated payouts for same bet
+
+        // TODO: look up bet by commitment
 
         (token, to, amount) = _getPayout(marketBlob, resultBlob, betBlob);
         if (amount > 0) {
             token.safeTransfer(to, amount);
         }
+
+        // TODO: event
     }
 
     /**
@@ -168,22 +245,39 @@ abstract contract MarketsBase is Context, MarketsErrors {
     /**
      * Hook that raises an error if the result does not make sense (e.g. total potential payout amount is wrong)
      */
-    function _verifyResult(MarketBlob calldata marketBlob, ResultBlob calldata resultBlob) internal view virtual;
+    function _verifyResult(
+        MarketCommitment marketCommitment,
+        MarketBlob calldata marketBlob,
+        ResultCommitment resultCommitment,
+        ResultBlob calldata resultBlob
+    ) internal view virtual;
 }
 
+// TODO: rename Parimutuel
 contract Markets is MarketsBase {
     struct MarketInfo {
         address creator;
-        uint256 deadline; // TODO: block or timestamp?
+        /**
+         * When a market is considered done
+         */
+        uint256 deadlineBlock;
         /**
          * Non-sequential market id decided off-chain
          */
         uint256 marketId;
+        uint256 numOutcomes;
     }
 
-    // TODO: call it BetBlob?
+    struct ResultInfo {
+        uint256 winningOption;
+        /**
+         * multiplier of the amount to get the reward on top of the original amount
+         */
+        UD60x18 normalization;
+    }
+
     struct BetHiddenInfo {
-        MarketCommitment MarketCommitment;
+        MarketCommitment marketCommitment;
         uint256 option;
         /**
          * random salt to ensure hash cannot be predicted
@@ -191,16 +285,46 @@ contract Markets is MarketsBase {
         uint256 salt;
     }
 
-    function _getPayout(MarketBlob calldata marketBlob, ResultBlob calldata resultBlob, BetBlob calldata betBlob)
+    struct BetInfo {
+        BetRequest request;
+        BetHiddenInfo hidden;
+    }
+
+    constructor(address admin) MarketsBase(admin) { }
+
+    function _verifyResult(
+        MarketCommitment marketCommitment,
+        MarketBlob calldata marketBlob,
+        ResultCommitment resultCommitment,
+        ResultBlob calldata resultBlob
+    ) internal view override {
+        MarketInfo memory marketInfo = abi.decode(marketBlob.data, (MarketInfo));
+        if (marketInfo.deadlineBlock < block.number) {
+            revert MarketsResultTooEarly(marketCommitment, block.number);
+        }
+
+        ResultInfo memory resultInfo = abi.decode(resultBlob.data, (ResultInfo));
+        if (resultInfo.winningOption >= marketInfo.numOutcomes) {
+            revert MarketsInvalidResult(marketCommitment, resultCommitment);
+        }
+    }
+
+    function _getPayout(MarketBlob calldata, ResultBlob calldata resultBlob, BetBlob calldata betBlob)
         internal
-        view
+        pure
         override
         returns (IERC20 token, address to, uint256 amount)
     {
-        // TODO: decode betBlob,
-        // get normalization factor from result blob
-        // TODO: might not need marketBlob here?
-    }
+        BetInfo memory betInfo = abi.decode(betBlob.data, (BetInfo));
+        ResultInfo memory resultInfo = abi.decode(resultBlob.data, (ResultInfo));
 
-    function _verifyResult(MarketBlob calldata marketBlob, ResultBlob calldata resultBlob) internal view override { }
+        // TODO: any more sanity checks?
+
+        if (betInfo.hidden.option == resultInfo.winningOption) {
+            token = betInfo.request.token;
+            to = betInfo.request.from;
+            // TODO: take care of fees
+            amount = betInfo.request.amount + unwrap(ud60x18(betInfo.request.amount) * resultInfo.normalization);
+        }
+    }
 }
--- test/Markets.t.sol
+++ test/Markets.t.sol
@@ -5,11 +5,24 @@ pragma solidity ^0.8.28;
 
 import { Test } from "forge-std/Test.sol";
 
-import { IERC20Errors, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20Errors, IERC20, ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { IERC165, ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
 import { ERC2771Forwarder } from "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import { UD60x18, ud60x18, unwrap, uUNIT, UNIT } from "@prb/math/UD60x18.sol";
+
+import {
+    MarketsBase,
+    Markets,
+    MarketBlob,
+    MarketCommitment,
+    BetBlob,
+    BetCommitment,
+    ResultBlob,
+    ResultCommitment
+} from "../contracts/Markets.sol";
 
 /// @dev Adapted from guide on testing EIP712 signatures for foundry:
 /// https://book.getfoundry.sh/tutorials/testing-eip712?highlight=712#testing-eip-712-signatures
@@ -66,8 +79,129 @@ contract SigUtils {
     }
 }
 
+contract MockERC20 is ERC20 {
+    constructor() ERC20("TEST", "TEST") { }
+
+    function mint(address account, uint256 value) external {
+        return _mint(account, value);
+    }
+}
+
 contract MarketsTest is Test {
-    function testBlank() public pure {
-        assert(true);
+    MockERC20 public erc20;
+    Markets public markets;
+
+    address public admin;
+    address public alice;
+    address public bob;
+    address public creator;
+    address public resultSigner;
+    uint256 public resultSignerPrivateKey;
+
+    uint256 public marketDeadlineBlock;
+    uint256 public submissionDeadlineBlock;
+
+    using MessageHashUtils for bytes32;
+
+    function setUp() public {
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
+        creator = makeAddr("creator");
+        admin = makeAddr("admin");
+        (resultSigner, resultSignerPrivateKey) = makeAddrAndKey("result-signer");
+
+        erc20 = new MockERC20();
+        markets = new Markets(admin);
+        submissionDeadlineBlock = block.number + 100;
+        marketDeadlineBlock = block.number + 1000;
+
+        // Set up permissions
+        bytes32 role = markets.RESULT_SIGNATURE_ROLE();
+        vm.prank(admin);
+        markets.grantRole(role, resultSigner);
+    }
+
+    function signCommitment(uint256 privateKey, bytes32 commitment) public pure returns (bytes memory sig) {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, commitment);
+        sig = abi.encodePacked(r, s, v); // as detailed in Openzeppelin ECDSA.recover
+    }
+
+    function signCommitment(ResultCommitment commitment) public view returns (bytes memory) {
+        return signCommitment(resultSignerPrivateKey, ResultCommitment.unwrap(commitment));
+    }
+
+    function placeBet(address user, MarketsBase.BetRequest memory request, BetCommitment betCommitment) public {
+        erc20.mint(user, request.amount);
+        vm.prank(user);
+        erc20.approve(address(markets), request.amount);
+
+        vm.prank(user);
+        markets.placeBet(request, betCommitment);
+    }
+
+    function testEndToEnd() public {
+        Markets.MarketInfo memory marketInfo =
+            Markets.MarketInfo({ creator: creator, deadlineBlock: marketDeadlineBlock, marketId: 0x42, numOutcomes: 2 });
+        MarketBlob memory marketBlob = MarketBlob({ data: abi.encode(marketInfo) });
+        MarketCommitment marketCommitment = MarketCommitment.wrap(keccak256(marketBlob.data));
+
+        // Prepare alice to bet
+        uint96 aliceBetAmount = 10e18;
+        Markets.BetInfo memory aliceBetInfo = Markets.BetInfo({
+            request: MarketsBase.BetRequest({
+                token: erc20,
+                amount: aliceBetAmount,
+                from: alice,
+                nonce: 0,
+                submissionDeadlineBlock: submissionDeadlineBlock
+            }),
+            hidden: Markets.BetHiddenInfo({ marketCommitment: marketCommitment, option: 1, salt: 0x42 })
+        });
+        BetBlob memory aliceBetBlob = BetBlob({ data: abi.encode(aliceBetInfo) });
+        BetCommitment aliceBetCommitment = BetCommitment.wrap(keccak256(aliceBetBlob.data));
+
+        // Approve erc20 and make bet
+        placeBet(alice, aliceBetInfo.request, aliceBetCommitment);
+        vm.assertEq(erc20.balanceOf(alice), 0, "Amount taken for bet");
+
+        // Prepare bob to bet
+        uint96 bobBetAmount = 20e18;
+        Markets.BetInfo memory bobBetInfo = abi.decode(aliceBetBlob.data, (Markets.BetInfo));
+        bobBetInfo.request.from = bob;
+        bobBetInfo.request.amount = bobBetAmount;
+        bobBetInfo.hidden.option = 0;
+        vm.assertNotEq(bobBetInfo.hidden.option, aliceBetInfo.hidden.option);
+        BetBlob memory bobBetBlob = BetBlob({ data: abi.encode(bobBetInfo) });
+        BetCommitment bobBetCommitment = BetCommitment.wrap(keccak256(bobBetBlob.data));
+
+        placeBet(bob, bobBetInfo.request, bobBetCommitment);
+        vm.assertEq(erc20.balanceOf(bob), 0, "Amount taken for bet");
+
+        // Reveal market result
+        Markets.ResultInfo memory resultInfo;
+        {
+            // Normalization is losing balances divided by winning pool balances
+            UD60x18 normalization = ud60x18(uUNIT * bobBetAmount / aliceBetAmount);
+            resultInfo = Markets.ResultInfo({
+                winningOption: aliceBetInfo.hidden.option, // alice should win
+                normalization: normalization
+            });
+        }
+        ResultBlob memory resultBlob = ResultBlob({ data: abi.encode(resultInfo) });
+        ResultCommitment resultCommitment = ResultCommitment.wrap(keccak256(resultBlob.data));
+
+        bytes memory resultSignature = signCommitment(resultCommitment);
+        markets.revealMarketResult(marketBlob, resultBlob, resultSignature);
+
+        // Reveal each bet. Alice should get back the whole pot
+        vm.assertEq(erc20.balanceOf(address(markets)), aliceBetAmount + bobBetAmount, "All money is in pool");
+
+        markets.revealBet(marketBlob, resultBlob, bobBetBlob);
+        vm.assertEq(erc20.balanceOf(address(markets)), aliceBetAmount + bobBetAmount);
+        vm.assertEq(erc20.balanceOf(address(bob)), 0, "Bob did not win");
+
+        markets.revealBet(marketBlob, resultBlob, aliceBetBlob);
+        vm.assertEq(erc20.balanceOf(address(markets)), 0, "Alice claimed her winnings");
+        vm.assertEq(erc20.balanceOf(address(alice)), aliceBetAmount + bobBetAmount, "Alice received her winnings");
     }
 }
